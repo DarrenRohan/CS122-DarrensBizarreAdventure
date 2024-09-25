7c7a859884394c450693c4c896913ff168d2131a
Darren <darrenrohan64@gmail.com>
2024-09-25 15:32:24 -0700

    Finished code

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ num1 = input("Enter number here: ")
 num2 = input("Enter number here: ") 
 num3 = input("Enter number here: ")
 
+if num1 > num2:
+    if num2 > num3:
+        print (num3)
+    else: 
+        print (num2)
+else:
+    if num1 > num3:
+        print (num3)
+    else:
+        print (num1)
   
   
